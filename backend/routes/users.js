e5f3c1311a4a721a3302ac259f8c2d318861eb69
Manisha <manishagaonkar7875@gmail.com>
2019-11-10 10:45:31 +0530

    added jwt token anf front end intrgration

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 router.post('/signup', (req, res, next)=>{
     bcrypt.hash(req.body.password, 10)
@@ -27,7 +28,40 @@ router.post('/signup', (req, res, next)=>{
 });
 
 router.post('/login', (req, res, next)=>{
+    let fetchedUser;
+    User.findOne({ email: req.body.email}).then(user => {
+        if (!user) {
+            return res.status(401).json({
+                message: "auth failed"
+            });
+        }
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+        if (!result) {
+            return res.status(401).json({
+                message: "auth failed"
+            });
+        }
 
+        const token = jwt.sign(
+            { email: fetchedUser.email, userId: fetchedUser._id },
+            "secret_this_should_be_longer",
+            { expiresIn: "1h" }
+        );
+
+        return res.status(200).json({
+            token: token,
+            expiresIn: 3600
+        });
+
+    })
+    .catch(err => {
+        return res.status(401).json({
+            message: "auth failed"
+        });
+    });
 });
 
 
--- src/app/auth/signup/singup.component.ts
+++ src/app/auth/signup/singup.component.ts
@@ -11,13 +11,10 @@ export class SingupComponent {
     constructor(public authService: AuthService){}
 
     onSignUp (form: NgForm) {
-        console.log(form.value);
         if (form.invalid) {
             return;
         }
-
         this.authService.createUser(form.value.email, form.value.password);
-        
     }
     
 }
