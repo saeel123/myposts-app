d61f6fa5bc80265a075590831d1c2ea8149c65c6
Manisha <manishagaonkar7875@gmail.com>
2019-11-10 08:30:38 +0530

    added api routes and user model

---
+++ backend/routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.post('/signup', (req, res, next)=>{
+
+});
+
+router.post('/login', (req, res, next)=>{
+
+});
+
+
+module.exports = router;
